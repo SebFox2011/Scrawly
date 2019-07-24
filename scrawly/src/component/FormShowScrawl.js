c865deb50da49f88aa869bdf1c10a4bb58d51969
Seb <sebastien.philippe5@gmail.com>
2019-07-24 14:19:16 +0200

    Ajout de gravatar pour gérer les icones des users

--- scrawly/src/component/FormShowScrawl.js
+++ scrawly/src/component/FormShowScrawl.js
@@ -1,11 +1,24 @@
 import React, {Component} from 'react';
+import md5 from "md5";
+
 
 class FormShowScrawl extends Component {
     render() {
+        const gravatar ='https://gravatar.com/avatar/' + md5('sebfox@gmail.com') + '?s=32&d=robohash';
+        const{title,slug,persons,choices}=this.props.scrawl;
+
         return (
             <div>
-                <h2>{this.props.scrawl.title}</h2>
-                <p>{this.props.scrawl.slug}</p>
+                <h2>{title}</h2>
+                <p>{slug}</p>
+                
+                <img src={gravatar} alt="Gravatar"/>
+                <p>{this.props.scrawl.persons[0].username}</p>
+                <p>{this.props.scrawl.persons[0].email}</p>
+
+
+
+
 
             </div>
         );
