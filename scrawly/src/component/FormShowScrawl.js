2e9b1711f532ee5eb9b10ce71c9022d2940d9d77
Seb <sebastien.philippe5@gmail.com>
2019-07-24 14:56:24 +0200

    affichage des personnes et des dates d'un poll

--- scrawly/src/component/FormShowScrawl.js
+++ scrawly/src/component/FormShowScrawl.js
@@ -4,21 +4,30 @@ import md5 from "md5";
 
 class FormShowScrawl extends Component {
     render() {
-        const gravatar ='https://gravatar.com/avatar/' + md5('sebfox@gmail.com') + '?s=32&d=robohash';
-        const{title,slug,persons,choices}=this.props.scrawl;
+        const gravatar = 'https://gravatar.com/avatar/' + md5('sebfox@gmail.com') + '?s=32&d=robohash';
+        const {title, slug, persons, choise} = this.props.scrawl;
 
+        const people = persons.map((person) =>
+            <p>{person.username} {person.email} </p>
+        );
+
+        const date = choise.map((choice) =>
+            <p>{choice.date} </p>
+        );
         return (
             <div>
                 <h2>{title}</h2>
                 <p>{slug}</p>
-                
-                <img src={gravatar} alt="Gravatar"/>
-                <p>{this.props.scrawl.persons[0].username}</p>
-                <p>{this.props.scrawl.persons[0].email}</p>
+                {this.props.scrawl.persons.map((persons, index) => {
+                    console.log("persons");
+                    return (<p key={index} persons={persons}/>)
 
+                })}
+                {people}
+                {date}
 
-
-
+                <p><img src={gravatar}
+                        alt="Gravatar"/>{this.props.scrawl.persons[0].username} {this.props.scrawl.persons[0].email}</p>
 
             </div>
         );
